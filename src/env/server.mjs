da7af7df3840d4a97d5abcdc7a46c04dca0f4de9
Alex <pauloalbuquerque@acm.org>
2022-09-01 19:28:46 -0300

    style: reformats project

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -9,22 +9,22 @@ import { env as clientEnv, formatErrors } from "./client.mjs";
 const _serverEnv = serverSchema.safeParse(process.env);
 
 if (_serverEnv.success === false) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
-  );
-  throw new Error("Invalid environment variables");
+	console.error(
+		"❌ Invalid environment variables:\n",
+		...formatErrors(_serverEnv.error.format()),
+	);
+	throw new Error("Invalid environment variables");
 }
 
 /**
  * Validate that server-side environment variables are not exposed to the client.
  */
 for (let key of Object.keys(_serverEnv.data)) {
-  if (key.startsWith("NEXT_PUBLIC_")) {
-    console.warn("❌ You are exposing a server-side env-variable:", key);
+	if (key.startsWith("NEXT_PUBLIC_")) {
+		console.warn("❌ You are exposing a server-side env-variable:", key);
 
-    throw new Error("You are exposing a server-side env-variable");
-  }
+		throw new Error("You are exposing a server-side env-variable");
+	}
 }
 
 export const env = { ..._serverEnv.data, ...clientEnv };
--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -1,21 +1,21 @@
-import { createRouter } from "./context";
-import { z } from "zod";
+import {createRouter} from "./context";
+import {z} from "zod";
 
 export const exampleRouter = createRouter()
-  .query("hello", {
-    input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
-    resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    },
-  })
-  .query("getAll", {
-    async resolve({ ctx }) {
-      return await ctx.prisma.example.findMany();
-    },
-  });
+	.query("hello", {
+		input: z
+			.object({
+				text: z.string().nullish(),
+			})
+			.nullish(),
+		resolve({input}) {
+			return {
+				greeting: `Hello ${input?.text ?? "world"}`,
+			};
+		},
+	})
+	.query("getAll", {
+		async resolve({ctx}) {
+			return await ctx.prisma.example.findMany();
+		},
+	});
