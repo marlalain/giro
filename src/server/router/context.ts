4b5f3b6f8a0eec787b9d101ea2697a489511ad9b
Alex <alex@alexaa.dev>
2022-09-01 19:28:46 -0300

    style: reformats project

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,12 +1,12 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { Session } from "next-auth";
-import { getServerAuthSession } from "../../server/common/get-server-auth-session";
-import { prisma } from "../db/client";
+import {Session} from "next-auth";
+import {getServerAuthSession} from "../../server/common/get-server-auth-session";
+import {prisma} from "../db/client";
 
 type CreateContextOptions = {
-  session: Session | null;
+	session: Session | null;
 };
 
 /** Use this helper for:
@@ -14,10 +14,10 @@ type CreateContextOptions = {
  * - trpc's `createSSGHelpers` where we don't have req/res
  **/
 export const createContextInner = async (opts: CreateContextOptions) => {
-  return {
-    session: opts.session,
-    prisma,
-  };
+	return {
+		session: opts.session,
+		prisma,
+	};
 };
 
 /**
@@ -25,16 +25,16 @@ export const createContextInner = async (opts: CreateContextOptions) => {
  * @link https://trpc.io/docs/context
  **/
 export const createContext = async (
-  opts: trpcNext.CreateNextContextOptions,
+	opts: trpcNext.CreateNextContextOptions,
 ) => {
-  const { req, res } = opts;
+	const {req, res} = opts;
 
-  // Get the session from the server using the unstable_getServerSession wrapper function
-  const session = await getServerAuthSession({ req, res });
+	// Get the session from the server using the unstable_getServerSession wrapper function
+	const session = await getServerAuthSession({req, res});
 
-  return await createContextInner({
-    session,
-  });
+	return await createContextInner({
+		session,
+	});
 };
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
