f0c419b717c4ac78987de6de67faa835147a9a92
Alex <alex@alexaa.dev>
2022-09-05 22:30:44 -0300

    feat(button): pink!

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,11 +2,13 @@ type Props = {
 	children: any;
 	onClick?: () => void;
 	className?: string;
+	submit?: boolean;
 };
 
-const Button = ({ children, className = '', ...props }: Props) => (
+const Button = ({ children, className = '', submit, ...props }: Props) => (
 	<button
-		className={`rounded-md border border-gray-300 bg-white px-4 py-1 hover:border-blue-500 focus:outline-none dark:border-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-50 dark:hover:border-blue-500 ${className}`}
+		className={`rounded-md border border-gray-300 bg-white px-4 py-1 hover:border-blue-500 focus:outline-none dark:border-gray-100 dark:bg-gray-700 dark:text-gray-50 dark:hover:border-blue-500 ${className}`}
+		type={submit ? 'submit' : 'button'}
 		{...props}
 	>
 		{children}
