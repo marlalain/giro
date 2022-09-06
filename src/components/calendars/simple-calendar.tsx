056120744eaf554bcd9fd7d3d4bd18d054e7f3db
Alex <pauloalbuquerque@acm.org>
2022-09-05 21:08:49 -0300

    feat(simple-calendar): updates calendar

--- src/components/calendars/simple-calendar.tsx
+++ src/components/calendars/simple-calendar.tsx
@@ -1,13 +1,6 @@
-import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/20/solid";
-import {useState} from "react";
-import moment from "moment/moment";
-
-type Day = {
-	date: string;
-	isCurrentMonth?: boolean;
-	isToday?: boolean;
-	isSelected?: boolean;
-};
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
+import { useState } from 'react';
+import moment from 'moment/moment';
 
 const classNames = (...classes: any[]) => classes.filter(Boolean).join(' ');
 
@@ -18,8 +11,8 @@ const SimpleCalendar = () => {
 
 	const daysToRender = 35;
 	const firstDayOfWeek = currentDate.clone().startOf('month').startOf('week');
-	const currentMonth = currentDate.format("MMMM");
-	const currentYear = currentDate.format("YYYY");
+	const currentMonth = currentDate.format('MMMM').toLowerCase();
+	const currentYear = currentDate.format('YYYY');
 
 	const days = new Array(daysToRender).fill(0).map((_, i) => {
 		const day = moment(firstDayOfWeek).add(++i, 'days');
@@ -27,7 +20,7 @@ const SimpleCalendar = () => {
 		return {
 			date: day.format('YYYY-MM-DD'),
 			isCurrentMonth: day.month() === currentDate.month(),
-			isToday: day.isSame(moment(), 'day'),
+			isToday: day.isSame(today.clone(), 'day'),
 			isSelected: day.isSame(selectedDay, 'day'),
 		};
 	});
@@ -35,25 +28,36 @@ const SimpleCalendar = () => {
 	return (
 		<div className="col-span-3 px-2 py-1.5">
 			<div className="flex items-center">
-				<h2 className="flex-auto font-medium text-gray-800">{currentMonth} {currentYear}</h2>
+				<h2 className="flex-auto font-medium text-gray-800">
+					{currentMonth} {currentYear}
+				</h2>
 
 				<button
-					onClick={() => setCurrentDate(currentDate.clone().subtract(1, 'month'))}
-					className="-my-1.5 flex flex-none items-center justify-center rounded-md p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
+					onClick={() =>
+						setCurrentDate(currentDate.clone().subtract(1, 'month'))
+					}
+					className="-my-1.5 flex flex-none items-center justify-center rounded-md p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+				>
 					<span className="sr-only">Previous month</span>
-					<ChevronLeftIcon className="h-5 w-5 lg:h-6 lg:w-6" aria-hidden="true"/>
+					<ChevronLeftIcon
+						className="h-5 w-5 lg:h-6 lg:w-6"
+						aria-hidden="true"
+					/>
 				</button>
 
 				<button
 					onClick={() => setCurrentDate(currentDate.clone().add(1, 'month'))}
-					className="5 -my-1 flex flex-none items-center justify-center rounded-md p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
+					className="-my-1.5 flex flex-none items-center justify-center rounded-md p-1 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+				>
 					<span className="sr-only">Next month</span>
-					<ChevronRightIcon className="h-5 w-5 lg:h-6 lg:w-6" aria-hidden="true"/>
+					<ChevronRightIcon
+						className="h-5 w-5 lg:h-6 lg:w-6"
+						aria-hidden="true"
+					/>
 				</button>
 			</div>
 
-			<div
-				className="mt-10 grid grid-cols-7 text-center text-xs leading-6 text-gray-500">
+			<div className="mt-10 grid grid-cols-7 text-center text-xs leading-6 text-gray-500">
 				<div>M</div>
 				<div>T</div>
 				<div>W</div>
@@ -64,31 +68,42 @@ const SimpleCalendar = () => {
 			</div>
 
 			<div className="mt-2 grid grid-cols-7 text-sm">
-				{days.map((day, i) => (
+				{days.map((day) => (
 					<div key={day.date} className="py-2">
 						<button
 							onClick={() => setSelectedDay(moment(day.date))}
 							className={classNames(
 								day.isSelected && 'text-white',
-								!day.isSelected && !day.isToday && day.isCurrentMonth && 'text-gray-900',
-								!day.isSelected && !day.isToday && !day.isCurrentMonth && 'text-gray-400',
+								!day.isSelected &&
+									!day.isToday &&
+									day.isCurrentMonth &&
+									'text-gray-900',
+								!day.isSelected &&
+									!day.isToday &&
+									!day.isCurrentMonth &&
+									'text-gray-400',
 								day.isSelected && day.isToday && 'bg-pink-600',
 								day.isSelected && !day.isToday && 'bg-pink-600',
 								!day.isSelected && 'hover:bg-pink-200',
 								(day.isSelected || day.isToday) && 'font-semibold',
-								'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
-							)}>
+								'mx-auto flex h-8 w-8 items-center justify-center rounded-full',
+							)}
+						>
 							<time
 								className={classNames(
-									day.isToday && 'border border-transparent px-1 border-2 border-b-pink-600',
+									day.isToday &&
+										'border border-2 border-transparent border-b-pink-600 px-1',
 								)}
-								dateTime={day.date}>{day.date.split('-').pop()?.replace(/^0/, '')}</time>
+								dateTime={day.date}
+							>
+								{moment(day.date).format('D')}
+							</time>
 						</button>
 					</div>
 				))}
 			</div>
 		</div>
 	);
-}
+};
 
 export default SimpleCalendar;
