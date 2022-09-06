2848dc1c5ef708bb9c989776b3519e04780abfcd
Alex <alex@alexaa.dev>
2022-09-06 17:36:15 -0300

    fix(button): ignore this one

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Button = ({ children, className = '', submit, ...props }: Props) => (
 	<button
-		className={`rounded-md border border-gray-300 bg-white px-4 py-1 hover:border-blue-500 focus:outline-none dark:border-gray-100 dark:bg-gray-700 dark:text-gray-50 dark:hover:border-blue-500 ${className}`}
+		className={`rounded-md border border-gray-300 bg-white px-4 py-1 hover:border-pink-500 focus:outline-none dark:border-gray-100 dark:bg-gray-700 dark:text-gray-50 dark:hover:border-pink-500 ${className}`}
 		type={submit ? 'submit' : 'button'}
 		{...props}
 	>
